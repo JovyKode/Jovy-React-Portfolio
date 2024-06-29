0ed0c7e0f5d034cc8777941c3ddf2f146e90127e
jovy <jovy@someone>
2024-06-29 10:09:46 -0400

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,21 +4,36 @@ import  porject_default from '../assets/jigar-panchal-8BAyCFRCHqY-unsplash.jpg'
 function Contact (){
     return (
         <main>
-        <h3>Projects</h3>
-        <p>I try to work on projects that grab interest that i don't think a regular person would try to build. Hope you like it :)</p>
-        
-        <div className='project-box'>
-            <img src={porject_default} alt="image of projrect" />
-            <div className='project-info'>
-                <p>Name : Classified <br/>
-                Project type: Website<br/>
-                Built : 100%<br/>
-                Owner : Raneil Simms<br/>
-                Status : OnGoing</p>
-                <button>info </button>
-                {/*<button> Go to site </button>*/}
-            </div>    
-        </div>
+            <h3>Projects</h3>
+            <p>Work none stop to reach the goal that you desire.</p>
+            <div className='projects'>
+                <div className='project-box'>
+                    <img src={porject_default} alt="image of projrect" />
+                    <div className='project-info'>
+                        <p>Name : Classified <br/>
+                        Project type: Website<br/>
+                        Built Participation : 100%<br/>
+                        Owner : Raneil Simms<br/>
+                        Status : temporyorary on halt</p>
+                        {/*<button> Go to site </button>*/}
+                    </div>  
+                </div>
+
+                <div className='project-box'>
+                    <img src={porject_default} alt="image of projrect" />
+                    <div className='project-info'>
+                        <p>Name : Classified <br/>
+                        Project type: Website<br/>
+                        Built participating : 100%<br/>
+                        Owner : Jowayne Henry<br/>
+                        Status : ongoing</p>
+                      
+                        {/*<button> Go to site </button>*/}
+                    </div>    
+                </div>
+
+           
+            </div>
         </main>
 
     )
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,25 @@
+import webicon from '../assets/website-hosting-icon.webp'
+import appbuild from '../assets/desktop-mobile-icon.webp'
+import  net from '../assets/pc-network-icon.webp'
+import mobilerepair from '../assets/pc-network-icon.webp'
+import office from '../assets/office-365-icon.webp'
+import windows from   '../assets/windows-icon.webp'
 
 const Services = () => {
     return (
         <main>
-            <h3>services</h3>
-            <p>update comeing soon .....</p>
+            <h3>Services</h3>
+            <p>Down below consist of Computer services i can provide depandinding on the state of the device\devices when looked at .We can talk about price when device is examined aswell.</p>
+            <div>
+                <ul>
+                    <li><img src={windows}/>Computer Repair ,Customization and parts Installation</li>
+                    <li><img src={office}/>Microsoft Office (word, excel, Powerpoint, Outlook, etc) </li>
+                    <li><img src={mobilerepair}>Phone Repair </li>
+                    <li><img src={net}/>Network Setup</li>
+                    <li><img src={webicon}/>Building Scalable Website</li>
+                    <li><img src={appbuild}/>Building mobile and Desktop apps</li> 
+                </ul>
+            </div>
         </main>
     )
 }
