3e8f56b5c3611355e6e28c48b802164bc515cb35
jovy <jovy@someone>
2024-06-29 10:33:23 -0400

    update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,7 @@
 import webicon from '../assets/website-hosting-icon.webp'
 import appbuild from '../assets/desktop-mobile-icon.webp'
 import  net from '../assets/pc-network-icon.webp'
-import mobilerepair from '../assets/pc-network-icon.webp'
+import mobilerepair from '../assets/mobile-portrait-mode-icon.png'
 import office from '../assets/office-365-icon.webp'
 import windows from   '../assets/windows-icon.webp'
 
@@ -10,11 +10,11 @@ const Services = () => {
         <main>
             <h3>Services</h3>
             <p>Down below consist of Computer services i can provide depandinding on the state of the device\devices when looked at .We can talk about price when device is examined aswell.</p>
-            <div>
+            <div className='services'>
                 <ul>
                     <li><img src={windows}/>Computer Repair ,Customization and parts Installation</li>
                     <li><img src={office}/>Microsoft Office (word, excel, Powerpoint, Outlook, etc) </li>
-                    <li><img src={mobilerepair}>Phone Repair </li>
+                    <li><img src={mobilerepair}/>Phone Repair </li>
                     <li><img src={net}/>Network Setup</li>
                     <li><img src={webicon}/>Building Scalable Website</li>
                     <li><img src={appbuild}/>Building mobile and Desktop apps</li> 
