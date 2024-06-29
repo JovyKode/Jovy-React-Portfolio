aaa22db9832e7527c904d0f4fdf071c034a3df24
jovy <jovy@someone>
2024-06-29 11:25:50 -0400

    Contact page update 1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,11 @@
 
 const Contact = () => {
     return (
-        <h1>Contact</h1>
+        <main>
+            <h3>Contact</h3>
+            <p> This page will be availaible later on today or ill decide to get rid of this page and just work with the social button above which will be updaed instead </p>
+        </main>
+        
     )
 }
 
