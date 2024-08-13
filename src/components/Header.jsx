414d05814408bf9431520cf3d0b767bcb3bc2875
jovy <jovy@someone>
2024-08-13 15:20:24 -0400

    social button fuctionallity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,49 @@
-//Header
-import React from 'react'
-import './Style.css'
-import { Link } from 'react-router-dom'
-import ig_icon from '../assets/ig-instagram-icon.png'
-import linkedin from '../assets/linkedin-app-icon.png'
-import twit from '../assets/x-social-media-logo-icon.png'
-import github from '../assets/github-icon.png'
+import React, { useState } from 'react';
+import './Style.css';
+import { Link } from 'react-router-dom';
+import ig_icon from '../assets/ig-instagram-icon.png';
+import linkedin from '../assets/linkedin-app-icon.png';
+import github from '../assets/github-icon.png';
 
+const Header = () => {
+  const [showSocialDropdown, setShowSocialDropdown] = useState(false);
 
-const  Header = () => {
-    return (
-        <>
-            <header >
-                <div  className='header'>
-                    
-                    <Link className="logo" to={"/"}>JOVY</Link>  
-                    <button className='social-button'>Socials</button>
-                  
+  const drop = () => {
+    setShowSocialDropdown(!showSocialDropdown);
+  };
+
+  return (
+    <>
+      <header>
+        <div className='header'>
+          <Link className="logo" to={"/"}>JOVY</Link>
+          <button className='social-button' onClick={drop}>Socials</button>
+          {showSocialDropdown && (
+            <div className='socialdrop'>
+              {/*ig */}
+             
+
+              {/*github */}
+              <a href="https://github.com/JovyKode">
+                <div>
+                  <img src={github} alt='github'/>
+                  Jovykode
                 </div>
-               
-            </header>
-           
-        </>
-    )
-}
+              </a>
 
+              {/*linkedin */}
+              <a href="https://www.linkedin.com/in/nathaniel-henry-b14a14240?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
+                <div>
+                  <img src={linkedin} alt='linkedin'/>
+                  <span style={{marginLeft : 5}}>jowayne</span>
+                </div>
+              </a>
+            </div>
+          )}
+        </div>
+      </header>
+    </>
+  );
+};
 
-
-
-
-export default Header
-
+export default Header;
